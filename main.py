940e714dc9e0c1649ea4db0274aaec53f42edb02
Maxym S <seredovichm@gmail.com>
2021-02-16 17:06:06 +0200

    almost_all_fix

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import math
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
+import folium
 
-def read_list(path,year):
+def read_list(path,year,coordinates):
     apr_loc_and_movies = {}
     with open(path,encoding='ISO-8859-1') as movie_info:
         for _ in range(14):
@@ -11,23 +12,40 @@ def read_list(path,year):
             line = line[0:-2]
             line = line.split('\t')
             year_and_name = take_year_name(line)
+            if not year_and_name:
+                continue
             if year_and_name[0] == year:
                 index = define_correct_adress(line)
-                if index == ' ':
+                if not index:
                     continue
-                apr_loc_and_movies[line[index]] = apr_loc_and_movies.get(line[index],[]) + [year_and_name[1]]
+                key_coordinates = define_cordinates(line[index])
+                if not key_coordinates:
+                    continue
+                distance = distance_between_dots(coordinates[1],key_coordinates[1],
+                                                 coordinates[0],key_coordinates[0])
+                movie_n = cool_name(year_and_name[1])
+                apr_loc_and_movies[(key_coordinates,distance)] = apr_loc_and_movies.get((key_coordinates,distance),[]) + [movie_n]
     return apr_loc_and_movies
 
 def take_year_name(line):
     '''
     return the year and name of movie from str
     '''
-    name_year = line[0]
-    year_index = name_year.find('" (')
-    year = name_year[year_index:]
-    name = name_year[:year_index+1]
-    year = int(year[3:7])
-    return year,name
+    try:
+        name_year = line[0]
+        for index in range(len(name_year)):
+            if name_year[index] == '(':
+                points = 0
+                for elem in name_year[index+1:index+5]:
+                    if elem.isdigit():
+                        points += 1
+                if points == 4:
+                    year = name_year[index+1:index+5]
+                    break
+        name = name_year[:index]
+        return int(year),name
+    except:
+        return False
 
 def define_correct_adress(line):
     try:
@@ -36,26 +54,54 @@ def define_correct_adress(line):
             index -= 1
         return index
     except IndexError:
-        return ' '
+        return False
+
+def cool_name(name:str):
+    bad_elem = ['#','"']
+    for elem in bad_elem:
+        name = name.replace(elem,'')
+    return name
 
 def distance_between_dots(lon_1,lon_2,lat_1,lat_2):
     fs_dod = (math.sin((lat_2 - lat_1)/2))**2
     sec_dod = (math.sin((lon_2 - lon_1)/2))**2
     fst_ex = fs_dod + math.cos(lat_1)*math.cos(lat_2)*sec_dod
-    result = 2 * 6000*math.asin(fst_ex)
+    result = 2 * 6371*math.asin(fst_ex)
     return result
 
 
 
 def define_cordinates(adress):
-    geolocator = Nominatim(user_agent='Maxym')
-    location = geolocator.geocode(adress)
-    return (location.latitude,location.longitude)
+    try:
+        geolocator = Nominatim(user_agent='Maxym')
+        geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
 
-# def 
+        location = geolocator.geocode(adress)
+        return (location.latitude,location.longitude)
+    except:
+        return False
+
+
+def get_10_points(all_cor:dict):
+    lst = list(all_cor.keys())
+    lst = sorted(lst,key=lambda info:info[1])
+    new_dict = {key[0]:set(all_cor[key]) for key in lst[:10]}
+    return new_dict
+
+def create_map(info:dict,coordinates:list):
+    map = folium.Map(location= coordinates,zoom_start=10)
+    fg = folium.FeatureGroup(name="Movies map")
+    for key in info:
+        fg.add_child(folium.Marker(location=key,
+                                    popup= str(info[key]),
+                                    icon=folium.Icon()))
+    map.add_child(fg)
+    map.save('map.html')
+
+
 
 if __name__ == '__main__':
-    # print(define_correct_adress(['"#Fuga" (2016)', '', '', '', '', '', 'Barra da Tijuca, Rio de Janeiro, Rio de Janeiro, Brazil', '(location']))
-    print(read_list('prob_lst',2016))
-    # print(define_cordinates('New York City, New York, US'))
-    # print(distance_between_dots(24.02324,-74.0060152,49.83826,40.7127281))
+    line = read_list('prob_lst',2012,(49.83826,24.02324 ))
+    info = get_10_points(line)
+    print(info)
+    create_map(info,[49.83826,24.02324])
